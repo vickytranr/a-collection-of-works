9f157cdc8b8a20f060c6009db5c5127f166c8e83
vickytranr <78326917+vickytranr@users.noreply.github.com>
2022-06-26 23:39:44 -0400

    updates

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
+//Drawing on html canvas from stack overflow:
+//https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
 var canvas, ctx, flag = false,
 prevX = 0,
 currX = 0,
 prevY = 0,
 currY = 0,
 dot_flag = false;
-
+ 
 var x = "rgb(42, 90, 115)",
 y = 10;
-
+ 
 function init() {
 canvas = document.getElementById('canvas');
 ctx = canvas.getContext("2d");
 ctx.canvas.width  = window.innerWidth;
-ctx.canvas.height = 650;
-
+ctx.canvas.height = 1000;
+ 
 canvas.addEventListener("mousemove", function (e) {
-    findxy('move', e)
+   findxy('move', e)
 }, false);
 canvas.addEventListener("mousedown", function (e) {
-    findxy('down', e)
+   findxy('down', e)
 }, false);
 canvas.addEventListener("mouseup", function (e) {
-    findxy('up', e)
+   findxy('up', e)
 }, false);
 canvas.addEventListener("mouseout", function (e) {
-    findxy('out', e)
+   findxy('out', e)
 }, false);
 }
-
+ 
 function color(obj) {
 switch (obj.id) {
-    case "pink":
-        x = "#fe938cff";
-        break;
-    case "tumbleweed":
-        x = "#e6b89cff";
-        break;
-    case "wheat":
-        x = "#ead2acff";
-        break;
-    case "pewter":
-        x = "#9cafb7ff";
-        break;
-    case "celadon":
-        x = "#4281a4ff";
-        break;
-    case "dark-celadon":
-        x = "rgb(42, 90, 115)";
-        break;
-    case "white":
-        x = "rgb(42, 90, 115)";
-        break;
+   case "pink":
+       x = "#fe938cff";
+       break;
+   case "tumbleweed":
+       x = "#e6b89cff";
+       break;
+   case "wheat":
+       x = "#ead2acff";
+       break;
+   case "pewter":
+       x = "#9cafb7ff";
+       break;
+   case "celadon":
+       x = "#4281a4ff";
+       break;
+   case "dark-celadon":
+       x = "rgb(42, 90, 115)";
+       break;
+   case "white":
+       x = "rgb(42, 90, 115)";
+       break;
 }
 if (x == "white") y = 10;
 else y = 10;
-
+ 
 }
-
+ 
 function draw() {
 ctx.beginPath();
 ctx.arc(prevX,prevY,y,0,2*Math.PI);
@@ -64,43 +66,43 @@ ctx.fillStyle = x;
 ctx.fill();
 ctx.closePath();
 }
-
+ 
 function erase() {
 var m = confirm("Want to clear");
 if (m) {
-    ctx.clearRect(0, 0, w, h);
-    document.getElementById("canvasimg").style.display = "none";
+   ctx.clearRect(0, 0, w, h);
+   document.getElementById("canvasimg").style.display = "none";
 }
 }
-
+ 
 function findxy(res, e) {
 if (res == 'down') {
-    prevX = currX;
-    prevY = currY;
-    currX = e.clientX - canvas.offsetLeft;
-    currY = e.clientY - canvas.offsetTop;
-
-    flag = true;
-    dot_flag = true;
-    if (dot_flag) {
-        ctx.beginPath();
-        ctx.arc(currX,currY,y,0,2*Math.PI);
-        ctx.fillStyle = x;
-        ctx.fill();
-        ctx.closePath();
-        dot_flag = false;
-    }
+   prevX = currX;
+   prevY = currY;
+   currX = e.clientX - canvas.getBoundingClientRect().left;
+   currY = e.clientY - canvas.getBoundingClientRect().top;
+ 
+   flag = true;
+   dot_flag = true;
+   if (dot_flag) {
+       ctx.beginPath();
+       ctx.arc(currX,currY,y,0,2*Math.PI);
+       ctx.fillStyle = x;
+       ctx.fill();
+       ctx.closePath();
+       dot_flag = false;
+   }
 }
 if (res == 'up' || res == "out") {
-    flag = false;
+   flag = false;
 }
 if (res == 'move') {
-    if (flag) {
-        prevX = currX;
-        prevY = currY;
-        currX = e.clientX - canvas.offsetLeft;
-        currY = e.clientY - canvas.offsetTop;
-        draw();
-    }
+   if (flag) {
+       prevX = currX;
+       prevY = currY;
+       currX = e.clientX - canvas.getBoundingClientRect().left;
+       currY = e.clientY - canvas.getBoundingClientRect().top;
+       draw();
+   }
 }
 }
